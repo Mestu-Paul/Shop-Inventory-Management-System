9ee64b84a9f4952731a5b727487b59c5a35a2b6c
Mestu Paul <paulmestu@gmail.com>
2023-01-23 12:46:33 +0600

    add staff manager design

---
+++ staff_manager.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+
+import color_code as color
+
+
+def back_home(main_frame):
+    print("back home")
+    main_frame.place_forget()
+def add_new_staff(main_frame):
+    pass
+def manage_stuff(main_frame):
+    pass
+def stuff_payment(main_frame):
+    pass
+def payment_report(main_frame):
+    pass
+def staff_manager(root):
+    main_frame = tk.Frame(root,bg=color.color2)
+    main_frame.place(relx=0,rely=0.172,relwidth=1,relheight=0.75)
+    staff_menu = tk.Menu(main_frame)
+    main_frame.config(menu=staff_menu)
+    # add_new_staff(main_frame)
+    # staff_menu.add_command(
+    #     label='Add New',
+    #     command=lambda:add_new_staff(main_frame)
+    # )
+    # staff_menu.add_command(
+    #     label='Manage Stuf',
+    #     command=lambda:manage_stuff(main_frame)
+    # )
+    # staff_menu.add_command(
+    #     label='Stuff Payment',
+    #     command=lambda:stuff_payment(main_frame)
+    # )
+    # staff_menu.add_command(
+    #     label='Payment Report',
+    #     command=lambda:payment_report(main_frame)
+    # )
+
+root = tk.Tk()
+root.geometry('1100x650+10+10')
+staff_manager(root)
+root.mainloop()
