1a4ef3f28cee32418c498ddbdb79dc0ea8274dc3
Mestu Paul <paulmestu@gmail.com>
2023-02-04 12:55:09 +0600

    update messagebox

--- help_functions.py
+++ help_functions.py
@@ -2,17 +2,41 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import re as regex
 
+def on_enter(event):
+    global btn
+    btn.config(bg='#dcfbf6',bd=2)
+    print('enter')
+    
+def on_leave(event):
+    global btn
+    btn.config(bg='#f1f1f1',bd=1)
+    print('leave')
+    
 def show_message(message_type,message):
-    messagebox = tk.Toplevel()
-    messagebox.geometry('300x200+200+100')
-    messagebox.resizable(False,False)
-    message_icon = ImageTk.PhotoImage(Image.open(f"img/{message_type}.png").resize((50,50)))
-    tk.Label(messagebox,image=message_icon).place(relx=0.4,rely=0.01)
-    text = tk.Text(messagebox,wrap=tk.WORD)
-    text.place(relx=0,rely=0.3,relwidth=1,relheight=0.55)
+    messageBox = tk.Toplevel()
+    screen_width = messageBox.winfo_screenwidth()
+    screen_height = messageBox.winfo_screenheight()
+    # calculate the position to center the frame
+    x = (screen_width - 200) // 2
+    y = (screen_height - 200) // 2
+    messageBox.geometry('300x200+{}+{}'.format(x, y))
+    messageBox.title(message_type)
+    messageBox.resizable(False,False)
+    messageBox.grab_set()
+    
+    message_icon = ImageTk.PhotoImage(Image.open("img/error.png").resize((50,50)))
+    tk.Label(messageBox,bg='#ffffff', image=message_icon).place(relx=0,rely=0.0,relwidth=0.3,relheight=0.75)
+
+    text = tk.Text(messageBox,wrap=tk.WORD,bd=0)
+    text.place(relx=0.3,rely=0,relwidth=1,relheight=0.75)
     text.insert(tk.END,message)
-    tk.Button(messagebox,text='Ok',command=messagebox.destroy).place(relx=0.46,rely=0.85,relwidth=0.15)
-    messagebox.mainloop()
+    global btn
+    btn = tk.Button(messageBox,text='Ok', bd=1, bg='#f1f1f1', relief=tk.SUNKEN, highlightbackground="red", command=messageBox.destroy)
+    btn.place(relx=0.7,rely=0.8,width=80)
+
+    btn.bind("<Enter>",on_enter)
+    btn.bind("<Leave>",on_leave)
+    messageBox.mainloop()
 
 def is_number(var):
     for v in var:
